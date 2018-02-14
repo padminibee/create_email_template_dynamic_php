2441f0761607c04f55a75d298d65ee2f0524eb11
padminibee <beerapadmini@gmail.com>
2018-02-14 17:44:08 +0530

    Create email_template.php

---
+++ email_template.php
@@ -0,0 +1,78 @@
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>Lausanne Tours and Travels</title>
+</head>
+<body style="margin: 0px;padding: 0px">
+<table cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td style="padding: 25px;background:#eee ">
+			<table cellpadding="0" cellspacing="0" border="0" style="background: #ffffff">
+				<tr align="center">
+				<td style="padding:10px 20px;"><div style="border-bottom:1px solid #d1b555;padding-bottom:15px;align:center;background-color:#1F1C61; "><img src="%logoimg%" alt="%logoimg_alt%" /></div></td>
+				</tr>
+				<tr>
+					<td style="-moz-hyphens: none;-webkit-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;word-break: break-word;-webkit-hyphens: none;-ms-text-size-adjust: 100%;hyphens: none;font-family:Open Sans,Arial,Helvetica,sans-serif;font-size:20px;line-height: 25px;color: #000;border-collapse: collapse;padding: 15px;padding:10px 20px">%FormTitle% !</td>
+				</tr>
+				<tr>
+					<td style="-moz-hyphens: none;-webkit-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;word-break: break-word;-webkit-hyphens: none;-ms-text-size-adjust: 100%;hyphens: none;font-family:Open Sans,Arial,Helvetica,sans-serif;font-size:15px;line-height: 25px;color: #000;border-collapse: collapse;padding:10px 20px">
+						Dear
+						<strong style="-moz-hyphens: none;-webkit-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;word-break: break-word;-webkit-hyphens: none;-ms-text-size-adjust: 100%;hyphens: none;font-family:Open Sans,Arial,Helvetica,sans-serif;font-size:18px;line-height: 25px;color: #ac5e2a;border-collapse: collapse;">%Name%,</strong>
+					</td>
+				</tr>				
+				<tr>		
+					<td style="-moz-hyphens: none;-webkit-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;word-break: break-word;-webkit-hyphens: none;-ms-text-size-adjust: 100%;hyphens: none;font-family:Open Sans,Arial,Helvetica,sans-serif;font-size:15px;line-height: 25px;color: #000;border-collapse: collapse;padding:10px 20px">
+						You have received a new Inquiry For %inquiryfor%. Below are the User details
+					</td>
+				</tr>
+				<tr>		
+					<td style="-moz-hyphens: none;-webkit-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;word-break: break-word;-webkit-hyphens: none;-ms-text-size-adjust: 100%;hyphens: none;font-family:Open Sans,Arial,Helvetica,sans-serif;font-size:14px;line-height:normal;color: #000;border-collapse: collapse;padding:10px 20px 10px 20px;">
+						<table cellpadding="0" cellspacing="0" border="0" width="100%" id="proxylisttable">
+							<tr>
+								<th align="left" style="border:1px solid #eee;padding:5px">Name</th>
+								<td align="left" style="border:1px solid #eee;padding:5px">%passenger-name%</td>
+							</tr>
+							<tr>
+								<th align="left" style="border:1px solid #eee;padding:5px">Mobile</th>
+								<td align="left" style="border:1px solid #eee;padding:5px">%mobile%</td>
+							</tr>	
+							<tr>
+								<th align="left" style="border:1px solid #eee;padding:5px">Email ID</th>
+								<td align="left" style="border:1px solid #eee;padding:5px">%email%</td>
+							</tr>							
+							
+								<tr>
+								<th align="left" style="border:1px solid #eee;padding:5px">%column1%</th>
+								<td align="left" style="border:1px solid #eee;padding:5px">%column1_val%</td>
+								</tr>
+								<tr>
+								<th align="left" style="border:1px solid #eee;padding:5px">%column2%</th>
+								<td align="left" style="border:1px solid #eee;padding:5px">%column2_val%</td>
+								</tr>
+								<tr>
+								<td align="left" style="border:1px solid #eee;padding:5px">%column3%</td>
+								<td align="left" style="border:1px solid #eee;padding:5px">%column3_val%</td>
+								</tr>
+								<tr>
+								<td align="left" style="border:1px solid #eee;padding:5px">%column4%</td>
+								<td align="left" style="border:1px solid #eee;padding:5px">%column4_val%</td>
+								</tr>
+								<tr>
+								<td align="left" style="border:1px solid #eee;padding:5px">%column5%</td>
+								<td align="left" style="border:1px solid #eee;padding:5px">%column5_val%</td>
+								</tr>
+								<tr>
+								<td align="left" style="border:1px solid #eee;padding:5px">%column6%</td>
+								<td align="left" style="border:1px solid #eee;padding:5px">%column6_val%</td>
+								</tr>
+															
+						</table>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
